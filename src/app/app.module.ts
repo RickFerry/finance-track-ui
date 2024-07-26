3e4fa0a1e5e664888bb993b3f3920526819a8aa1
Ricardo <rick_ferry1@hotmail.com>
2024-07-26 16:31:18 -0300

    14ª aula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ButtonModule } from 'primeng/components/button/button';
-import { DataTableModule } from 'primeng/components/datatable/datatable';
-import { InputTextModule } from 'primeng/components/inputtext/inputtext';
-import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
-import { TooltipModule } from 'primeng/components/tooltip/tooltip';
-import { CalendarModule } from 'primeng/components/calendar/calendar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {ButtonModule} from 'primeng/components/button/button';
+import {DataTableModule} from 'primeng/components/datatable/datatable';
+import {InputTextModule} from 'primeng/components/inputtext/inputtext';
+import {InputTextareaModule} from 'primeng/components/inputtextarea/inputtextarea';
+import {TooltipModule} from 'primeng/components/tooltip/tooltip';
+import {CalendarModule} from 'primeng/components/calendar/calendar';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
-import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import {AppComponent} from './app.component';
+import {LancamentoCadastroComponent} from './lancamento-cadastro/lancamento-cadastro.component';
+import {LancamentosPesquisaComponent} from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {PessoasPesquisaComponent} from './pessoas-pesquisa/pessoas-pesquisa.component';
+import {SelectButtonModule} from 'primeng/components/selectbutton/selectbutton';
 
 @NgModule({
   declarations: [
@@ -30,9 +31,11 @@ import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.co
     DataTableModule,
     TooltipModule,
     InputTextareaModule,
-    CalendarModule
+    CalendarModule,
+    SelectButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
